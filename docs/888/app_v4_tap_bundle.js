b69f6672cd1938d36121734a416a243b886b1456
diewland <diewland@gmail.com>
2024-09-12 17:58:34 +0700

    prevent cache

--- docs/888/app_v4_tap_bundle.js
+++ docs/888/app_v4_tap_bundle.js
@@ -7,7 +7,7 @@ function load_script(src, callback=null) {
 load_script('/888/config.js?t=' + +(new Date()), _  => { // 1. load config (no cache)
   load_script('/888/abi_v4_tap.js', _ => {               // 2. load contract abi
     load_script('/888/abi_erc20.js', _ => {              // 3. load erc20 abi
-      load_script('/888/app_v4_tap.js?r=240911');        // 4. load app
+      load_script('/888/app_v4_tap.js?r=240912');        // 4. load app
     });
   });
 });
